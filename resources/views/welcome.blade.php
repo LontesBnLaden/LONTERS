00be3fe5748903124f100a0a1d484fd27c9596b0
LontesBnLaden <ptrujillogarcia6@gmail.com>
2023-10-06 18:31:20 -0500

    auxilio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<button type="button" class="btn btn-primary"><a href="mi1vistas.blade.php">mi1vistas</a></button>
+<button type="button" class="btn btn-primary"><a href="mi2vistas.blade.php">mi2vistas</a></button>
+<button type="button" class="btn btn-primary"><a href="mi3vistas.blade.php">mi3vistas</a></button>
+<button type="button" class="btn btn-primary"><a href="mi4vistas.blade.php">mi4vistas</a></button>
+<button type="button" class="btn btn-primary"><a href="mi5vistas.blade.php">mi5vistas</a></button>
+</body>
+</html>
